275d645aa90d19c7ebeed59b121158ef67caa60e
yyyannnn <349168872@gapps.yrdsb.ca>
2024-09-11 13:11:50 -0400

    Set Rocket location

--- Rocket.java
+++ Rocket.java
@@ -13,5 +13,19 @@ public class Rocket extends Actor
     public void act()
     {
         move(3);
+        
+        if(getX() <= 0){
+            resetLocation();
+        }
+    }
+    
+    public void resetLocation(){
+        int num = Greenfoot.getRandomNumber(2);
+        
+        if(num == 0){
+            setLocation(600,100);
+        }else{
+            setLocation(600,300);
+        }
     }
 }
